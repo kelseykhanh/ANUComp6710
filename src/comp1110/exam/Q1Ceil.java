134674157c484457e959ef6ee29ad055988423e1
u7836873 <u7836873@anu.edu.au>
2024-06-12 16:21:44 +1000

    Q1 Ceil

--- src/comp1110/exam/Q1Ceil.java
+++ src/comp1110/exam/Q1Ceil.java
@@ -1,5 +1,9 @@
 package comp1110.exam;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.io.*;
 /**
  * COMP1110 Final Exam, Question 1.1
  *
@@ -13,12 +17,32 @@ public class Q1Ceil {
      * If there is no value in the array that is less than
      * ceil, return (ceil+1).
      *
-     * @param in    An array of integers
+     * @param in   An array of integers
      * @param ceil a target value to search for in the array
      * @return the largest value in the array that is smaller than ceil,
      * or ceil+1 if there is no such value
      */
     public static int findLess(int[] in, int ceil) {
-        return 0; // FIXME complete this method
+
+
+        int max = Integer.MIN_VALUE;
+        boolean found = false;
+
+        for (int value : in) {
+            if (value < ceil) {
+                if (value > max) {
+                    max = value;
+                    found = true;
+                }
+            }
+        }
+
+        if (!found) {
+            return ceil + 1;
+        }
+
+        return max;
     }
 }
+
+
